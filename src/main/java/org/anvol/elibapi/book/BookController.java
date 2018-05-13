73a0ea44231036da45db8cf91c70da1f27dffcd7
Volodymyr Andriychak <andriychak@users.noreply.github.com>
2018-05-13 12:30:48 +0300

    initial commit

--- src/main/java/org/anvol/elibapi/book/BookController.java
+++ src/main/java/org/anvol/elibapi/book/BookController.java
@@ -3,22 +3,30 @@ package org.anvol.elibapi.book;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.anvol.elibapi.author.Author;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BookController {
 
+    @RequestMapping("/books")
     public List<Book> getAllBooks() {
-	Author author = new Author();
-	author.setName("Author name");
-	Set<Author> authorSet = new HashSet<Author>();
 
-	authorSet.add(author);
-
-	return Arrays.asList(new Book("Кобзар", "Вірші", authorSet));
+	return Arrays.asList(new Book("Кобзар", "Вірші", new HashSet<Author>()),
+		new Book("Незвичайні пригоди в лісовій школі", "Роман",
+			new HashSet<Author>()),
+		new Book("Лісова пісня", "Поема", new HashSet<Author>()),
+		new Book("Украдене щастя", "Повість", new HashSet<Author>()),
+		new Book("Незнайомка з країни Сонячних Зайчиків", "Роман",
+			new HashSet<Author>()),
+		new Book("Лис Микита", "Поема", new HashSet<Author>()),
+		new Book("Бояриня", "Поема", new HashSet<Author>()),
+		new Book("Тореадори з Васюківки", "Роман",
+			new HashSet<Author>()),
+		new Book("В країні Місячних Зайчиків", "Роман",
+			new HashSet<Author>()));
     }
 
 }
